f5035759ebd56a155edc808c53b6337c4ffd61bb
bluesky112233 <55771745+bluesky112233@users.noreply.github.com>
2019-09-27 15:12:47 -0400

    added

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <ctype.h>
+#include "dictionary.h"
+
+node* hashtable[HASH_SIZE];
+
+
+int main(int argc,char *argv[]) 
+{
+
+    if (argc!=3)
+        {
+                printf("Unexpected number of arguments.  Usage: spell_check <file_to_check> <dictionary_file>\n");
+                return -1;
+        }
+    //printf("arg[1]: %s arg[2] %s",argv[1],argv[2]);
+
+    
+    load_dictionary(argv[2],hashtable);
+    FILE * fp = fopen(argv[1], "r"); 
+    char * misspelled[1000];
+    check_words (fp,hashtable,misspelled);
+
+}
